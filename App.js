31684843f7d614693914122847b09ff99fb4ea91
jaesu968 <mazterk968@gmail.com>
2025-10-30 09:55:30 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+// --- Main screens ---
+
+// --- Onboarding screens ---
+
+// --- App ---
+
+const App = () => (
+  null
+);
+
+export default App;
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 8,
+  },
+  title: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+# ReactNativeProject_InstagramLikeApp3
